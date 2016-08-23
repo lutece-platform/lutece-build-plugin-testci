7790ad31112376bf896d3602d9195ccf65553349
Jon Harper <jon.harper87@gmail.com>
2016-08-23 17:51:44 +0200

    Add a simple business class to run tests in db

---
+++ src/sql/plugins/testci/plugin/create_db_testci.sql
@@ -0,0 +1,11 @@
+
+--
+-- Structure for table testci_testci
+--
+
+DROP TABLE IF EXISTS testci_testci;
+CREATE TABLE testci_testci (
+id_test_c_i int NOT NULL,
+name varchar(50) default '' NOT NULL,
+PRIMARY KEY (id_test_c_i)
+);
